fe0eea906ab4f3e8a76dbc06ea0f898b9507a84b
goodname40 <kalebmay22024@gmail.com>
2024-12-03 01:59:21 -0600

    0.1 First Release
    First ever release of website. New Music coming soon. Really soon

---
+++ kdubprod/kdub.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kdubinthemix</title>
+    <link rel="stylesheet" type="text/css" href="mainstyle.css"></link>
+</head>
+<body style="margin: 0%; padding: 0%;">
+    <div id="maindiv">
+        <div class="kdub_div_2">
+        <h1 class="kdub_main_title">Kdub</h1>
+    </div>
+    </div>
+</body>
+</html>
